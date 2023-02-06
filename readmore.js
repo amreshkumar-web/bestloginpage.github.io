ce0762b9b661a52c82a799a0bb08613148107c6b
amreshkumar-web <121627475+amreshkumar-web@users.noreply.github.com>
2023-02-06 22:52:52 +0530

    added index file

---
+++ readmore.js
@@ -0,0 +1,106 @@
+var content1=document.getElementById("box-child2");
+var content2=document.getElementById("box-child3");
+var content3=document.getElementById("box-child4");
+var content4=document.getElementById("box-child1");
+var content5=document.getElementById("tickgif");
+var content6=document.getElementById("logindone");
+var content7=document.getElementById("registerdone");
+
+function second(){
+/* content1.style.display="none";
+ content2.style.display="block";
+ content3.style.display="none";*/
+ content1.style.marginLeft="0rem"
+ content1.style.transition="1s";
+ 
+ li2.style.color="green";
+ li3.style.color="rgb(0,0,0,30%)";
+ li1.style.color="rgb(0,0,0,30%)";
+ content3.style.marginTop="0%"
+    content3.style.transition="1s";
+    content4.style.backgroundColor="";
+    content3.style.borderRadius="800px";
+    content1.style.borderRadius="800px";
+    content5.style.top="100%";
+    content6.style.top="100%";
+    content7.style.top="100%";
+    content7.style.transition="0.5s";
+    content1.style.marginTop="0";
+
+}
+function third(){
+    /*content1.style.display="none";
+    content2.style.display="none";
+    content3.style.display="block";*/
+    
+    content3.style.borderRadius="10px";
+    content1.style.marginLeft="-80rem"
+    content1.style.transition="1s";
+    li3.style.color="rgb(208, 21, 21)";
+    li2.style.color="rgb(0,0,0,30%)";
+ li1.style.color="rgb(0,0,0,30%)";
+ content1.style.transform="rotateZ(0deg)"
+    content1.style.borderRadius="800px";
+    content3.style.marginTop="0%"
+    content3.style.transition="1s";
+    content4.style.backgroundColor="";
+    content5.style.top="100%";
+    content6.style.top="100%";
+    content7.style.top="100%";
+    content7.style.transition="0.5s";
+    content1.style.marginTop="0";
+    
+   }
+   function first(){
+    /*content1.style.display="block";
+    content2.style.display="none";
+    content3.style.display="none";*/
+    
+    content1.style.marginLeft="80rem"
+    content1.style.transition="1s";
+    
+    content1.style.borderRadius="10px";
+    li1.style.color="goldenrod";
+    li2.style.color="rgb(0,0,0,30%)";
+ li3.style.color="rgb(0,0,0,30%)";
+ content3.style.marginTop="0%"
+    content3.style.transition="1s";
+    content4.style.backgroundColor="";
+    content3.style.borderRadius="800px";
+    content5.style.top="100%";
+    content6.style.top="100%";
+    content7.style.top="100%";
+    content7.style.transition="0.5s";
+    content1.style.marginTop="0";
+   
+   }
+   //---------------------------------------------------
+   function tick(){
+    content4.style.backgroundColor="rgb(0,0,0,30%)";
+    content4.style.transition="2s";
+    content3.style.marginTop="100%"
+    content3.style.transition="1s";
+    content5.style.top="45%";
+    content5.style.transition="1s";
+    content6.style.top="55%";
+    content6.style.transition="2s";
+    content7.style.top="100%";
+    content7.style.transition="0.5s";
+    content1.style.marginTop="0";
+    
+   }
+
+
+
+   //--------------------------------
+
+   function registerdone(){
+    content4.style.backgroundColor="rgb(0,0,0,30%)";
+    content4.style.transition="2s";
+    content5.style.top="45%";
+    content5.style.transition="1s";
+    content1.style.marginTop="-100%"
+    content1.style.transition="1s";
+    content7.style.top="55%";
+    content7.style.transition="2s";
+   }
